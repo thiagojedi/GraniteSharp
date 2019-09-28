461782c3a1aac14179acc0c55c2eb896bff9f521
Jedi Thiago <thiagojedi@live.it>
2019-09-28 13:03:58 -0300

    Upgrade to new versions of .net

--- Demo.Core/Program.cs
+++ Demo.Core/Program.cs
@@ -5,6 +5,6 @@ namespace Demo.Core
 {
     internal static class Program
     {
-        public static void Main(string[] args) => ((Application) new GraniteDemo()).Run();
+        public static void Main(string[] args) => (new GraniteDemo() as Application).Run();
     }
 }
--- Demo.Framework/Program.cs
+++ Demo.Framework/Program.cs
@@ -1,13 +1,9 @@
-﻿using Demo.Views;
-using Demo.Views.SettingsView;
-using GLib;
-using Gtk;
-using Application = GLib.Application;
+﻿using Application = GLib.Application;
 
 namespace Demo.Framework
 {
     internal static class Program
     {
-        private static int Main(string[] args) => ((Application) new GraniteDemo()).Run();
+        private static int Main(string[] args) => (new GraniteDemo() as Application).Run();
     }
 }
